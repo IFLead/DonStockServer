03d12307f6a205d5f57d82136b7372cdea2be6bd
IFLead <IGOS.321@gmail.com>
2018-01-13 18:39:58 +0300

    URL

--- DonStockServer/urls.py
+++ DonStockServer/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from REST import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shops/', views.ShopList.as_view()),
+    path('auth/', include('')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
