17aad007f57d3f05ace2024d1b67900fb5c4c5aa
Gumbeat <leo2598@yandex.ua>
2018-03-08 18:40:44 +0300

    auth with django 1.11.7

--- DonStockServer/urls.py
+++ DonStockServer/urls.py
@@ -14,19 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
 from django.views.generic import TemplateView
-from rest_framework.urlpatterns import format_suffix_patterns
+from REST.views import check_token
 from REST import views
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('shops/', views.ShopList.as_view()),
-                  path('auth/', include('rest_framework_social_oauth2.urls')),
-                  path('', TemplateView.as_view(template_name='index.html')),
-                  path('service-worker.js', (
-                  TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )),
-                       name='service-worker.js'),
+	              # path('admin/', admin.site.urls),
+	              # path('shops/', views.ShopList.as_view()),
+	              # path('auth/', include('rest_framework_social_oauth2.urls')),
+	              # path('', TemplateView.as_view(template_name='index.html')),
+	              # path('service-worker.js', (
+		           #   TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )),
+		           #   name='service-worker.js'),
+	              # path('api/login/', include('rest_social_auth.urls_jwt')),
+	              # path('api/login/', include('rest_social_auth.urls_token')),
+	              # path('api/login/', include('rest_social_auth.urls_session')),
+				url(r'^admin/', admin.site.urls),
+	            url(r'^shops/', views.ShopList.as_view()),
+	            url(r'^', TemplateView.as_view(template_name='index.html')),
+	            url(r'^service-worker.js', (
+				TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )),
+				name='service-worker.js'),
+	            url(r'^api/login/', include('rest_social_auth.urls_jwt')),
+	            url(r'^api/login/', include('rest_social_auth.urls_token')),
+	            url(r'^api/login/', include('rest_social_auth.urls_session')),
+				url(r'^auth/', include('rest_framework_social_oauth2.urls')),
+                url(r'api/check/', check_token),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
--- REST/views.py
+++ REST/views.py
@@ -1,10 +1,13 @@
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-import json
+
 from Voting.models import Vote
 from .models import Shop
 from .serializers import ShopSerializer
@@ -31,22 +34,29 @@ class Votes(APIView):
 	@method_decorator(login_required)
 	def post(self, request, format=None):
 		# if if_authorized(request.user) and 'action' in request.POST and 'shop' in request.POST:
-			action = request.data.get('action')
-			shop_id = request.data.get('shop')
-			try:
-				shop = Shop.objects.get(id=shop_id)
-			except:
-				return Response({'status': 'Invalid data'})
-			if action:
-				status = shop.votes.up(request.user.id)
-			else:
-				status = shop.votes.down(request.user.id)
-			return Response(
-				{'status': 'OK', 'rating': shop.calculate_vote_score, 'likes': shop.likes,
-					'dislikes': shop.dislikes, 'vote_status': status[1]})
+		action = request.data.get('action')
+		shop_id = request.data.get('shop')
+		try:
+			shop = Shop.objects.get(id=shop_id)
+		except:
+			return Response({'status': 'Invalid data'})
+		if action:
+			status = shop.votes.up(request.user.id)
+		else:
+			status = shop.votes.down(request.user.id)
+		return Response(
+			{'status': 'OK', 'rating': shop.calculate_vote_score, 'likes': shop.likes,
+				'dislikes': shop.dislikes, 'vote_status': status[1]})
 
 
 def if_authorized(user):
 	if user.is_authenticated:
 		return user.get_full_name()
 	return False
+
+
+@csrf_exempt
+@api_view(['POST'])
+def check_token(request, format=None):
+	token = Token.objects.filter(key=request.data['token']).exists()
+	return JsonResponse({"status": token})
